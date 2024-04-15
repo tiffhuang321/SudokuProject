373310bc179ad5d873ac2d4c1d20daec235328c2
tiffh <tiffhuang321@gmail.com>
2024-04-15 19:16:58 -0400

    cell.py

---
+++ cell.py
@@ -0,0 +1,16 @@
+class Cell:
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.sketched_value = value
+
+    def draw(self):
+        # reference draw_chips() function in main_gui.py
+        pass
